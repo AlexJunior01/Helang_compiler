d1544a7aafdfd4142b4ac825747ae2ecf94c3f9a
Alex Junior <alexmomijunior@gmail.com>
2021-09-22 21:08:59 -0300

    finalizando analisador sintático

--- src/main/Compiler.java
+++ src/main/Compiler.java
@@ -54,144 +54,150 @@ public class Compiler {
 		while ((ch = input[tokenPos]) == ' ' || ch == '\r' || ch == '\t' || ch == '\n') {
 			++tokenPos;
 		}
-        if(Character.isLetter(ch)){
-            StringBuffer ident = new StringBuffer();
+        if(ch == '\0') {
+          token = Symbol.EOF;
+        } else {
+            if(Character.isLetter(ch)){
+                StringBuffer ident = new StringBuffer();
 
-            while (Character.isLetter(input[tokenPos]) || Character.isDigit(input[tokenPos])){
-                ident.append(input[tokenPos]);
-                ++tokenPos;
-            }
+                while (Character.isLetter(input[tokenPos]) || Character.isDigit(input[tokenPos])){
+                    ident.append(input[tokenPos]);
+                    ++tokenPos;
+                }
 
-            stringValue = ident.toString();
-            Symbol value = keywordsTable.get(stringValue);
-            if (value == null){
-                token = Symbol.IDENT;
+                stringValue = ident.toString();
+                Symbol value = keywordsTable.get(stringValue);
+                if (value == null){
+                    token = Symbol.IDENT;
+                } else {
+                    token = value;
+                }
+            } else if (Character.isDigit(ch)) {
+                StringBuffer number = new StringBuffer();
+                while (Character.isDigit(input[tokenPos])) {
+                    number.append(input[tokenPos]);
+                    tokenPos++;
+                }
+                token = Symbol.NUMBER;
+
+                try {
+                    numberValue = Integer.parseInt(number.toString());
+                } catch (NumberFormatException e) {
+                    error("NÃºmero muito grande");
+                }
             } else {
-                token = value;
+                switch(ch){
+                    case ';':
+                        token = Symbol.PONTO_VIRGULA;
+                        ++tokenPos;
+                        break;
+                    case '{':
+                        token = Symbol.ABRE_CHAVES;
+                        ++tokenPos;
+                        break;
+                    case '}':
+                        token = Symbol.FECHA_CHAVES;
+                        ++tokenPos;
+                        break;
+                    case '(':
+                        token = Symbol.ABRE_PARANTESES;
+                        ++tokenPos;
+                        break;
+                    case ')':
+                        token = Symbol.FECHA_PARENTESES;
+                        ++tokenPos;
+                        break;
+                    case '=':
+                        if (input[tokenPos+1] == '='){
+                            token = Symbol.IGUAL;
+                            tokenPos += 2;
+                            break;
+                        }
+                        token = Symbol.ATRIBUICAO;
+                        ++tokenPos;
+                        break;
+                    case '.':
+                        if (input[tokenPos +1 ] == '.'){
+                            token = Symbol.DOIS_PONTOS;
+                            tokenPos += 2;
+                            break;
+                        }
+                        error("Esperado token = '.'");
+                        break;
+                    case '+':
+                        token = Symbol.MAIS;
+                        ++tokenPos;
+                        break;
+                    case '-':
+                        token = Symbol.MENOS;
+                        ++tokenPos;
+                        break;
+                    case '*':
+                        token = Symbol.MULTIPLICACAO;
+                        ++tokenPos;
+                        break;
+                    case '/':
+                        token = Symbol.DIVISAO;
+                        ++tokenPos;
+                        break;
+                    case '%':
+                        token = Symbol.MOD;
+                        ++tokenPos;
+                        break;
+                    case '!':
+                        if(input[tokenPos + 1] == '='){
+                            token = Symbol.DIFERENTE;
+                            tokenPos += 2;
+                            break;
+                        }
+                        token = Symbol.EXCLAMACAO;
+                        ++tokenPos;
+                        break;
+                    case '<':
+                        if(input[tokenPos + 1] == '='){
+                            token = Symbol.MENOR_IGUAL;
+                            tokenPos += 2;
+                            break;
+                        }
+                        token = Symbol.MENOR;
+                        ++tokenPos;
+                        break;
+                    case '>':
+                        if(input[tokenPos + 1] == '='){
+                            token = Symbol.MAIOR_IGUAL;
+                            tokenPos += 2;
+                            break;
+                        }
+                        token = Symbol.MAIOR;
+                        ++tokenPos;
+                        break;
+                    case '|':
+                        if(input[tokenPos + 1] == '|'){
+                            token = Symbol.OR;
+                            tokenPos += 2;
+                            break;
+                        }
+                        error("Token '|' esperado.");
+                        break;
+                    case '&':
+                        if(input[tokenPos + 1] == '&'){
+                            token = Symbol.AND;
+                            tokenPos += 2;
+                            break;
+                        }
+                        error("Token '&' esperado.");
+                        break;
+                    default:
+                        error("Invalida char");
+                }
             }
-        } else if (Character.isDigit(ch)) {
-            StringBuffer number = new StringBuffer();
-            while (Character.isDigit(input[tokenPos])) {
-                number.append(input[tokenPos]);
-                tokenPos++;
-            }
-            token = Symbol.NUMBER;
-
-            try {
-                numberValue = Integer.parseInt(number.toString());
-            } catch (NumberFormatException e) {
-                error("NÃºmero muito grande");
-            }
-        }
-        switch(ch){
-            case ';':
-                token = Symbol.PONTO_VIRGULA;
-                ++tokenPos;
-                break;
-            case '{':
-                token = Symbol.ABRE_CHAVES;
-                ++tokenPos;
-                break;
-            case '}':
-                token = Symbol.FECHA_CHAVES;
-                ++tokenPos;
-                break;
-            case '(':
-                token = Symbol.ABRE_PARANTESES;
-                ++tokenPos;
-                break;
-            case ')':
-                token = Symbol.FECHA_PARENTESES;
-                ++tokenPos;
-                break;
-            case '=':
-                if (input[tokenPos+1] == '='){
-                    token = Symbol.IGUAL;
-                    tokenPos += 2;
-                    break;
-                }
-                token = Symbol.ATRIBUICAO;
-                ++tokenPos;
-                break;
-            case '.':
-                if (input[tokenPos +1 ] == '.'){
-                    token = Symbol.DOIS_PONTOS;
-                    tokenPos += 2;
-                    break;
-                }
-                error("Esperado token = '.'");
-                break;
-            case '+':
-                token = Symbol.MAIS;
-                ++tokenPos;
-                break;
-            case '-':
-                token = Symbol.MENOS;
-                ++tokenPos;
-                break;
-            case '*':
-                token = Symbol.MULTIPLICACAO;
-                ++tokenPos;
-                break;
-            case '/':
-                token = Symbol.DIVISAO;
-                ++tokenPos;
-                break;
-            case '%':
-                token = Symbol.MOD;
-                ++tokenPos;
-                break;
-            case '!':
-                if(input[tokenPos + 1] == '='){
-                    token = Symbol.DIFERENTE;
-                    tokenPos += 2;
-                    break;
-                }
-                token = Symbol.EXCLAMACAO;
-                ++tokenPos;
-                break;
-            case '<':
-                if(input[tokenPos + 1] == '='){
-                    token = Symbol.MENOR_IGUAL;
-                    tokenPos += 2;
-                    break;
-                }
-                token = Symbol.MENOR;
-                ++tokenPos;
-                break;
-            case '>':
-                if(input[tokenPos + 1] == '='){
-                    token = Symbol.MAIOR_IGUAL;
-                    tokenPos += 2;
-                    break;
-                }
-                token = Symbol.MAIOR;
-                ++tokenPos;
-                break;
-            case '|':
-                if(input[tokenPos + 1] == '|'){
-                    token = Symbol.OR;
-                    tokenPos += 2;
-                    break;
-                }
-                error("Token '|' esperado.");
-                break;
-            case '&':
-                if(input[tokenPos + 1] == '&'){
-                    token = Symbol.AND;
-                    tokenPos += 2;
-                    break;
-                }
-                error("Token '&' esperado.");
-                break;
-            default:
-                error("Invalida char");
         }
 	}
 	
     public void compile( char []p_input ) {
         input = p_input;
         tokenPos = 0;
+        input[input.length - 1] = '\0';
         nextToken();
         program();
 //        int result = expr();
@@ -232,6 +238,115 @@ public class Compiler {
         }
     }
 
+    private void whileStat() {
+        nextToken();
+
+        expr();
+        statList();
+    }
+
+    /*
+        PrintlnStat ::= "println" Expr ";"
+     */
+    private void printlnStat() {
+        nextToken();
+        expr();
+
+        if(token != Symbol.PONTO_VIRGULA) {
+            error("';' esperado");
+        }
+        nextToken();
+    }
+
+    /*
+        PrintStat ::= "print" Expr ";"
+     */
+    private void printStat() {
+        nextToken();
+        expr();
+
+        if(token != Symbol.PONTO_VIRGULA) {
+            error("';' esperado");
+        }
+        nextToken();
+    }
+
+    /*
+        ForStat ::= "for" Id "in" Expr ".." Expr StatList
+     */
+    private void forStat() {
+        nextToken();
+
+        if(token != Symbol.IDENT) {
+            error("VariÃ¡vel esperado");
+        }
+        String iterador = stringValue;
+        nextToken();
+
+        if(token != Symbol.IN) {
+            error("'in' esperado");
+        }
+        nextToken();
+
+        expr();
+        if(token != Symbol.DOIS_PONTOS) {
+            error("'..' esperado");
+        }
+        nextToken();
+
+        expr();
+        statList();
+    }
+
+    /*
+        IfStat ::= "if" Expr StatList [
+                    "else" StatList ]
+     */
+    private void ifStat() {
+        nextToken();
+        expr();
+
+        statList();
+
+        if (token == Symbol.ELSE) {
+            nextToken();
+            statList();
+        }
+    }
+
+    /*
+        StatList ::= "{" { Stat } "}"
+     */
+    private void statList() {
+        if(token != Symbol.ABRE_CHAVES) {
+            error("'{' esperado");
+        }
+        nextToken();
+        while(token != Symbol.FECHA_CHAVES){
+            stat();
+        }
+        nextToken();
+    }
+
+    /*
+        AssignStat ::= Ident "=" Expr ";"
+     */
+    private void assignStat() {
+        String ident = stringValue;
+        nextToken();
+
+        if (token != Symbol.ATRIBUICAO) {
+            error("'=' esperado");
+        }
+        nextToken();
+
+        expr();
+        if (token != Symbol.PONTO_VIRGULA) {
+            error("';' esperado");
+        }
+        nextToken();
+    }
+
     /*
         VarList ::= { "var" Int Ident ";" }
      */
@@ -259,12 +374,11 @@ public class Compiler {
     /*
     	Expr ::= AndExpr [ "||" AndExpr ]
     */
-    
     private void expr() {
     	andExpr();
-    	nextToken();
-    	
+
     	if(token == Symbol.OR) {
+            nextToken();
     		andExpr();
     	}
     }
@@ -272,12 +386,11 @@ public class Compiler {
     /*
      	AndExpr ::= RelExpr [ "&&" RelExpr ]
     */
-
     private void andExpr() {
 		relExpr();
-		nextToken();
 		
 		if(token == Symbol.AND) {
+            nextToken();
 			relExpr();
     	}
 	}
@@ -288,10 +401,10 @@ public class Compiler {
     
 	private void relExpr() {
 		addExpr();
-		nextToken();
 		
 		if(token == Symbol.MENOR || token == Symbol.MAIOR || token == Symbol.MENOR_IGUAL || 
 				token == Symbol.MAIOR_IGUAL || token == Symbol.IGUAL || token == Symbol.DIFERENTE) {
+            nextToken();
 			addExpr();
 		}
 	}
@@ -299,12 +412,11 @@ public class Compiler {
 	/*
 		AddExpr ::= MultExpr { AddOp MultExpr }
 	*/
-
 	private void addExpr() {
 		multExpr();
-		nextToken();
 		
 		while(token == Symbol.MAIS || token == Symbol.MENOS ) {
+            nextToken();
 			multExpr();
 		}
 	}
@@ -312,18 +424,17 @@ public class Compiler {
 	/*
 		MultExpr ::= SimpleExpr { MultOp SimpleExpr }
 	*/
-
 	private void multExpr() {
 		simpleExpr();
-		nextToken();
 		
 		while(token == Symbol.MULTIPLICACAO || token == Symbol.DIVISAO || token == Symbol.MOD) {
+            nextToken();
 			simpleExpr();
 		}
 	}
 	
 	/*
-		SimpleExpr ::= Number | ’(’ Expr ’)’ | "!" SimpleExpr | AddOp SimpleExpr | Ident
+		SimpleExpr ::= Number | ï¿½(ï¿½ Expr ï¿½)ï¿½ | "!" SimpleExpr | AddOp SimpleExpr | Ident
 	*/
 	
 	private void simpleExpr() {
@@ -331,22 +442,36 @@ public class Compiler {
 		if(token == Symbol.NUMBER) {
 			number();
 		} else if(token == Symbol.ABRE_PARANTESES) {
+            nextToken();
 			expr();
 			if(token != Symbol.FECHA_PARENTESES) {
 				error("')' esperado");
 			}
+            nextToken();
 		} else if(token == Symbol.EXCLAMACAO) {
+            nextToken();
 			simpleExpr();
 		} else if(token == Symbol.MAIS || token == Symbol.MENOS) {
+            nextToken();
 			simpleExpr();
 		} else if(token == Symbol.IDENT) {
-			
+			String ident = this.stringValue;
+            nextToken();
 		} else {
 			error("simpleExpr esperado");
 		}	
 	}
 
-	private void error(String msg) {
+    private void number() {
+        if(token == Symbol.MAIS || token == Symbol.MENOS) {
+            nextToken();
+        }
+
+        int numero = numberValue;
+        nextToken();
+    }
+
+    private void error(String msg) {
         if ( tokenPos == 0 )
           tokenPos = 1;
         else
--- src/main/Main.java
+++ src/main/Main.java
@@ -3,13 +3,29 @@ package main;
 public class Main {
 	
 	public static void main( String []args ) {
-        char []input = ("var Int s;\r\n"
-        		+ "var Int i;\r\n"
-        		+ "s = 0;\r\n"
-        		+ "for j in 1..100 {\r\n"
-        		+ "s = s + j\r\n"
-        		+ "}\r\n"
-        		+ "println s;").toCharArray();
+        char []input = ("var Int i;\n" +
+				"var Int soma;\n" +
+				"var Int somaFor;\n" +
+				"var Int n;\n" +
+				"var Int verd;\n" +
+				"n = 100;\n" +
+				"soma = 0;\n" +
+				"i = 0;\n" +
+				"verd = 2;\n" +
+				"while i < n && !!verd { \n" +
+				"  if i%2 == (0+0*0/1) {\n" +
+				"\tsoma = (--soma) + i*i;\n" +
+				"  }\n" +
+				"  i = i + 1;\n" +
+				"}\n" +
+				"somaFor = 0;\n" +
+				"for k in 0..100 {\n" +
+				"  if i%2 == 0 {\n" +
+				"\tsomaFor = somaFor + k*k;\n" +
+				"  }\n" +
+				"}\n" +
+				"println soma;\n" +
+				"println somaFor; ").toCharArray();
         
         Compiler compiler = new Compiler();
 
